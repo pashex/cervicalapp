adbf75a3df374428337136df159b30fd2d7c31c5
Pavel Golenkov <pashex@mail.ru>
2015-02-17 15:51:55 +0500

    Add index action template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
     @page = Rhinoart::Page.find_by_path('index')
+    @articles = Rhinoart::Page.where(ptype: 'article', parent_id: @page.id).order(:position)
   end
 
   def internal
